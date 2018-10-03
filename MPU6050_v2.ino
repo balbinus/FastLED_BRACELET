09f250da206d90491a8e4bc96fd508f4285293ad
Vincent Tabard <vincent@balbinus.net>
2018-10-04 01:30:20 +0200

    HSV2RGB (rainbow method) + Passive Mode is the One True Mode, all hail Passive Mode

--- MPU6050_v2.ino
+++ MPU6050_v2.ino
@@ -421,7 +421,6 @@ void readBytes(uint8_t address, uint8_t subAddress, uint8_t count,
     }                                                                          // Put read results in the Rx buffer
 }
 
-#if ACTIVE_MODE == 0
 volatile uint32_t motion_detected = 0;
 volatile bool int_cleared = true;
 void flashLed()
@@ -429,7 +428,6 @@ void flashLed()
     motion_detected = millis();
     int_cleared = false;
 }
-#endif
 
 void setup()
 {
@@ -437,10 +435,8 @@ void setup()
     Serial.begin(38400);
 
     // Set up the interrupt pin, its set as active high, push-pull
-#if ACTIVE_MODE == 0
     pinMode(intPin, INPUT);
     digitalWrite(intPin, LOW);
-#endif
     
     // Builtin LED
     pinMode(LED_BUILTIN, OUTPUT);
@@ -480,12 +476,8 @@ void setup()
 
         calibrateMPU6050(accelBias);                                 // Calibrate gyro and accelerometers, load biases in bias registers
         initMPU6050();
-#if ACTIVE_MODE == 0
         attachInterrupt(digitalPinToInterrupt(intPin), flashLed, RISING);
         Serial.println("MPU6050 initialized for passive data mode....");        // Initialize device for passive mode read of acclerometer
-#else
-        Serial.println("MPU6050 initialized for active data mode....");        // Initialize device for active mode read of acclerometer, gyroscope, and temperature
-#endif
     }
     else
     {
@@ -495,10 +487,27 @@ void setup()
     }
 }
 
-__attribute__ ((always_inline)) static inline uint8_t scale8_LEAVING_R1_DIRTY( uint8_t i, fract8 scale)
+__attribute__ ((always_inline)) static inline uint8_t scale8( uint8_t i, fract8 scale)
 {
     return (((uint16_t)i) * ((uint16_t)(scale)+1)) >> 8;
 }
+#define scale8_LEAVING_R1_DIRTY(X,Y) scale8(X,Y)
+
+///  The "video" version of scale8 guarantees that the output will
+///  be only be zero if one or both of the inputs are zero.  If both
+///  inputs are non-zero, the output is guaranteed to be non-zero.
+///  This makes for better 'video'/LED dimming, at the cost of
+///  several additional cycles.
+__attribute__ ((always_inline)) static inline uint8_t scale8_video( uint8_t i, fract8 scale)
+{
+    uint8_t j = (((int)i * (int)scale) >> 8) + ((i&&scale)?1:0);
+    // uint8_t nonzeroscale = (scale != 0) ? 1 : 0;
+    // uint8_t j = (i == 0) ? 0 : (((int)i * (int)(scale) ) >> 8) + nonzeroscale;
+    return j;
+}
+#define scale8_video_LEAVING_R1_DIRTY(X,Y) scale8_video(X,Y)
+
+#define cleanup_R1() 0
 
 /// ease8InOutCubic: 8-bit cubic ease-in / ease-out function
 ///                 Takes around 18 cycles on AVR
@@ -548,13 +557,9 @@ static uint8_t cubicwave8(uint8_t in)
     return /*ease8InOutCubic(*/triwave8(in)/*)*/;
 }
 
-#if ACTIVE_MODE == 1
-bool led_state = false;
-#endif
 uint8_t color_index = 0;
 void loop()
 {
-#if ACTIVE_MODE == 0
     uint32_t now = millis();
     if (now > 2000 && now - motion_detected < 2000)
     {
@@ -585,6 +590,7 @@ void loop()
 #define Color(r,g,b) (((uint32_t) (r) << 16) | ((uint32_t) (g) << 8) | (b))
 uint32_t Wheel(byte WheelPos)
 {
+#ifdef WHEEL_BY_ADAFRUIT
     WheelPos = 255 - WheelPos;
     if(WheelPos < 85)
     {
@@ -600,45 +606,228 @@ uint32_t Wheel(byte WheelPos)
         WheelPos -= 170;
         return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
     }
+#else
+    return hsv2rgb_rainbow(WheelPos, 0xFF, 0xFF);
+#endif
 }
 
-void active_loop()
+// Sometimes the compiler will do clever things to reduce
+// code size that result in a net slowdown, if it thinks that
+// a variable is not used in a certain location.
+// This macro does its best to convince the compiler that
+// the variable is used in this location, to help control
+// code motion and de-duplication that would result in a slowdown.
+#define FORCE_REFERENCE(var)  asm volatile( "" : : "r" (var) )
+
+
+#define K255 255
+#define K171 171
+#define K170 170
+#define K85  85
+
+uint32_t hsv2rgb_rainbow(uint8_t hue, uint8_t sat, uint8_t val)
 {
-#endif
-    // If data ready bit set, all data registers have new data
-    if (readByte(MPU6050_ADDRESS, INT_STATUS) != 0)
-    {                                                                          // check if data ready interrupt
-        readAccelData(accelCount);                                             // Read the x/y/z adc values
-        getAres();
-
-        // Now we'll calculate the accleration value into actual g's
-        ax = (float) accelCount[0] * aRes - accelBias[0];                      // get actual g value, this depends on scale being set
-        ay = (float) accelCount[1] * aRes - accelBias[1];
-        az = (float) accelCount[2] * aRes - accelBias[2];
-    }
-
-#if ACTIVE_MODE == 1
-    uint32_t deltat = millis() - count;
-    if (deltat > 500)
-#endif
+    // Yellow has a higher inherent brightness than
+    // any other color; 'pure' yellow is perceived to
+    // be 93% as bright as white.  In order to make
+    // yellow appear the correct relative brightness,
+    // it has to be rendered brighter than all other
+    // colors.
+    // Level Y1 is a moderate boost, the default.
+    // Level Y2 is a strong boost.
+    const uint8_t Y1 = 1;
+    const uint8_t Y2 = 0;
+    
+    // G2: Whether to divide all greens by two.
+    // Depends GREATLY on your particular LEDs
+    const uint8_t G2 = 0;
+    
+    // Gscale: what to scale green down by.
+    // Depends GREATLY on your particular LEDs
+    //~ const uint8_t Gscale = 0;
+    
+    
+    //~ uint8_t hue = hsv.hue;
+    //~ uint8_t sat = hsv.sat;
+    //~ uint8_t val = hsv.val;
+    
+    uint8_t offset = hue & 0x1F; // 0..31
+    
+    // offset8 = offset * 8
+    uint8_t offset8 = offset;
     {
-#ifdef DEBUG_PRINT_SERIAL
-        // Print acceleration values in milligs!
-        Serial.print(1000 * ax);
-        Serial.print(" ");
-        Serial.print(1000 * ay);
-        Serial.print(" ");
-        Serial.print(1000 * az);
-        Serial.println(" ");
-#elif ACTIVE_MODE == 1
-        led_state = !led_state;
-        digitalWrite(LED_BUILTIN, led_state);
-#endif
-
-#if ACTIVE_MODE == 1
-        count = millis();
+#if defined(__AVR__)
+        // Left to its own devices, gcc turns "x <<= 3" into a loop
+        // It's much faster and smaller to just do three single-bit shifts
+        // So this business is to force that.
+        offset8 <<= 1;
+        asm volatile("");
+        offset8 <<= 1;
+        asm volatile("");
+        offset8 <<= 1;
+#else
+        // On ARM and other non-AVR platforms, we just shift 3.
+        offset8 <<= 3;
 #endif
     }
+    
+    uint8_t third = scale8( offset8, (256 / 3)); // max = 85
+    
+    uint8_t r, g, b;
+    
+    if( ! (hue & 0x80) ) {
+        // 0XX
+        if( ! (hue & 0x40) ) {
+            // 00X
+            //section 0-1
+            if( ! (hue & 0x20) ) {
+                // 000
+                //case 0: // R -> O
+                r = K255 - third;
+                g = third;
+                b = 0;
+                FORCE_REFERENCE(b);
+            } else {
+                // 001
+                //case 1: // O -> Y
+                if( Y1 ) {
+                    r = K171;
+                    g = K85 + third ;
+                    b = 0;
+                    FORCE_REFERENCE(b);
+                }
+                if( Y2 ) {
+                    r = K170 + third;
+                    //uint8_t twothirds = (third << 1);
+                    uint8_t twothirds = scale8( offset8, ((256 * 2) / 3)); // max=170
+                    g = K85 + twothirds;
+                    b = 0;
+                    FORCE_REFERENCE(b);
+                }
+            }
+        } else {
+            //01X
+            // section 2-3
+            if( !  (hue & 0x20) ) {
+                // 010
+                //case 2: // Y -> G
+                if( Y1 ) {
+                    //uint8_t twothirds = (third << 1);
+                    uint8_t twothirds = scale8( offset8, ((256 * 2) / 3)); // max=170
+                    r = K171 - twothirds;
+                    g = K170 + third;
+                    b = 0;
+                    FORCE_REFERENCE(b);
+                }
+                if( Y2 ) {
+                    r = K255 - offset8;
+                    g = K255;
+                    b = 0;
+                    FORCE_REFERENCE(b);
+                }
+            } else {
+                // 011
+                // case 3: // G -> A
+                r = 0;
+                FORCE_REFERENCE(r);
+                g = K255 - third;
+                b = third;
+            }
+        }
+    } else {
+        // section 4-7
+        // 1XX
+        if( ! (hue & 0x40) ) {
+            // 10X
+            if( ! ( hue & 0x20) ) {
+                // 100
+                //case 4: // A -> B
+                r = 0;
+                FORCE_REFERENCE(r);
+                //uint8_t twothirds = (third << 1);
+                uint8_t twothirds = scale8( offset8, ((256 * 2) / 3)); // max=170
+                g = K171 - twothirds; //K170?
+                b = K85  + twothirds;
+                
+            } else {
+                // 101
+                //case 5: // B -> P
+                r = third;
+                g = 0;
+                FORCE_REFERENCE(g);
+                b = K255 - third;
+                
+            }
+        } else {
+            if( !  (hue & 0x20)  ) {
+                // 110
+                //case 6: // P -- K
+                r = K85 + third;
+                g = 0;
+                FORCE_REFERENCE(g);
+                b = K171 - third;
+                
+            } else {
+                // 111
+                //case 7: // K -> R
+                r = K170 + third;
+                g = 0;
+                FORCE_REFERENCE(g);
+                b = K85 - third;
+                
+            }
+        }
+    }
+    
+    // This is one of the good places to scale the green down,
+    // although the client can scale green down as well.
+    if( G2 ) g = g >> 1;
+    //~ if( Gscale ) g = scale8_video_LEAVING_R1_DIRTY( g, Gscale);
+    
+    // Scale down colors if we're desaturated at all
+    // and add the brightness_floor to r, g, and b.
+    if( sat != 255 ) {
+        if( sat == 0) {
+            r = 255; b = 255; g = 255;
+        } else {
+            //nscale8x3_video( r, g, b, sat);
+            if( r ) r = scale8_LEAVING_R1_DIRTY( r, sat);
+            if( g ) g = scale8_LEAVING_R1_DIRTY( g, sat);
+            if( b ) b = scale8_LEAVING_R1_DIRTY( b, sat);
+            cleanup_R1();
+            
+            uint8_t desat = 255 - sat;
+            desat = scale8( desat, desat);
+            
+            uint8_t brightness_floor = desat;
+            r += brightness_floor;
+            g += brightness_floor;
+            b += brightness_floor;
+        }
+    }
+    
+    // Now scale everything down if we're at value < 255.
+    if( val != 255 ) {
+        
+        val = scale8_video_LEAVING_R1_DIRTY( val, val);
+        if( val == 0 ) {
+            r=0; g=0; b=0;
+        } else {
+            // nscale8x3_video( r, g, b, val);
+            if( r ) r = scale8_LEAVING_R1_DIRTY( r, val);
+            if( g ) g = scale8_LEAVING_R1_DIRTY( g, val);
+            if( b ) b = scale8_LEAVING_R1_DIRTY( b, val);
+            cleanup_R1();
+        }
+    }
+    
+    // Here we have the old AVR "missing std X+n" problem again
+    // It turns out that fixing it winds up costing more than
+    // not fixing it.
+    // To paraphrase Dr Bronner, profile! profile! profile!
+    //asm volatile(  ""  :  :  : "r26", "r27" );
+    //asm volatile (" movw r30, r26 \n" : : : "r30", "r31");
+    return Color(r, g, b);
 }
 
 //===================================================================================================================
